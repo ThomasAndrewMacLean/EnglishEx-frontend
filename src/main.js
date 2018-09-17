8b1ce916fc385e1cbfb86f8f06154a228d0c7c03
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2018-09-17 22:15:46 +0200

    setup vue sentry 🦅

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router/router";
 import store from "./store/store";
+import Raven from "raven-js";
+import RavenVue from "raven-js/plugins/vue";
 import "./registerServiceWorker";
+
+Raven.config("https://39def78bbaa6426c91ffc5c48d5c245c@sentry.io/1282651")
+  .addPlugin(RavenVue, Vue)
+  .install();
 
 Vue.config.productionTip = false;
 
