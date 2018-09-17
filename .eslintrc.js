aacffec41dad4764fb3e2e9980951b40ffe1f474
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2018-09-18 00:16:33 +0200

    single quotes ftw

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,8 @@ module.exports = {
     "no-multi-spaces": ["error", { exceptions: { Property: false } }],
     "key-spacing": ["error"],
     "no-multiple-empty-lines": "error",
-    "space-in-parens": ["error", "never"]
+    "space-in-parens": ["error", "never"],
+    quotes: ["error", "single"]
   },
   parserOptions: {
     parser: "babel-eslint"
--- src/main.js
+++ src/main.js
@@ -1,29 +1,30 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router/router";
-import store from "./store/store";
-import Raven from "raven-js";
-import RavenVue from "raven-js/plugins/vue";
-import VueAnalytics from "vue-analytics";
-import "./registerServiceWorker";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router/router';
+import store from './store/store';
+import Raven from 'raven-js';
+import RavenVue from 'raven-js/plugins/vue';
+import VueAnalytics from 'vue-analytics';
+import './registerServiceWorker';
 
 //SET UP GOOGLE ANALYTICS AND SENTRY ERROR LOGGING
-console.log("Setting up Sentry 🤖");
-if (window.location.host === "end-training.com") {
-  Raven.config("https://39def78bbaa6426c91ffc5c48d5c245c@sentry.io/1282651")
-    .addPlugin(RavenVue, Vue)
-    .install();
-  // Raven.captureException("hllo world! 🤖");
+console.log('Setting up Sentry 🤖');
+if (window.location.host === 'end-training.com') {
+    Raven.config('https://39def78bbaa6426c91ffc5c48d5c245c@sentry.io/1282651')
 
-  Vue.use(VueAnalytics, {
-    id: "UA-110018468-6",
-    router
-  });
+        .addPlugin(RavenVue, Vue)
+        .install();
+    // Raven.captureException("hllo world! 🤖");
+
+    Vue.use(VueAnalytics, {
+        id: 'UA-110018468-6',
+        router
+    });
 }
 //Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app');
