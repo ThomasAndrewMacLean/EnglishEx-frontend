246ec5ec56f9ddfa42c6a6fcc9ef475ed4eb1ca3
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2018-12-18 23:55:07 +0100

    no sort?

--- src/views/Course.vue
+++ src/views/Course.vue
@@ -14,7 +14,7 @@
                     <ul>
                         <li class="ex-list" :class="{ 'is-active': exercise && exercise._id === e.id }" :key="e.id"
                             v-for="e in exercises
-                            .sort((a,b) => parseInt(a.order) - parseInt(b.order))
+                            //.sort((a,b) => parseInt(a.order) - parseInt(b.order))
                             .filter(ex => ex.tagName === tag)"
                             @click="getExercise(e.id)">
                             {{e.title}}
